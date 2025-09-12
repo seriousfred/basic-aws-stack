7ff7c34ad9f72beba6b6a4e97765b146234af67a
Frederico Klein <fred.adm@gmail.com>
2025-09-12 20:06:51 -0300

    let's use a prefix, can be anything up to 20 chars

--- aws/variables.tf
+++ aws/variables.tf
@@ -7,3 +7,12 @@ variable "aws_region" {
   description = "AWS_REGION"
   type        = string
 }
+
+variable "prefix" {
+  description = "Some prefix for the names of the resources to be created"
+  type        = string
+  validation {
+    condition     = length(var.prefix) < 20
+    error_message = "20 chars is enough for a prefix"
+  }
+}
