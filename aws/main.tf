df2759b34d5863d722cc9d4d6b7ba970ab55fcd3
Frederico Klein <fred.adm@gmail.com>
2025-09-14 08:41:11 -0300

    format

--- aws/main.tf
+++ aws/main.tf
@@ -1,11 +1,8 @@
 
 # providers
-
 provider "aws" {
-  
   profile = var.aws_profile
   region  = var.aws_region
-
 }
 
 # utilities
@@ -13,40 +10,36 @@ resource "random_id" "RANDOM_ID" {
   byte_length = "6"
 }
 
-
-
-
 # modules
+# network
 module "net" {
   source = "./mods/net"
-  vpc_id = var.vpc_id # will create resources if not empty
-  prefix  = var.prefix
+  vpc_id = var.vpc_id
+  prefix = var.prefix
   cidr   = "10.42.0.0/16"
 }
 
-
-
 # rds
 module "rds" {
-  source = "./mods/rds"
-  prefix = var.prefix
-  vpc_id = module.net.vpc_id
-  subnets = module.net.data_subnets
+  source          = "./mods/rds"
+  prefix          = var.prefix
+  vpc_id          = module.net.vpc_id
+  subnets         = module.net.data_subnets
   allowed_subnets = module.net.private_subnets
 }
 
 # load balancer
 module "alb" {
-  source = "./mods/alb"
+  source     = "./mods/alb"
   create_alb = true
-  prefix = var.prefix
-  vpc_id = module.net.vpc_id
-  subnets = module.net.public_subnets
+  prefix     = var.prefix
+  vpc_id     = module.net.vpc_id
+  subnets    = module.net.public_subnets
 }
 
 # s3
 module "s3" {
-  source = "./mods/s3"
+  source      = "./mods/s3"
   bucket_name = "${var.prefix}-${var.aws_region}-${random_id.RANDOM_ID.hex}"
 }
 
@@ -56,10 +49,9 @@ module "sns" {
   topic_name = "${var.prefix}-alerts-${random_id.RANDOM_ID.hex}"
 }
 
-
 # ecr
 module "ecr" {
-  source = "./mods/ecr"
+  source    = "./mods/ecr"
   repo_name = "${var.prefix}/services-${random_id.RANDOM_ID.hex}"
 }
 
@@ -71,28 +63,28 @@ module "ecs_cluster" {
 
 # task definition
 module "ecs_task_def" {
-  source = "./mods/ecs/task-def"
-  prefix = var.prefix
-  name = "service-${random_id.RANDOM_ID.hex}"
-  repo = module.ecr.ecr_repository_url
+  source     = "./mods/ecs/task-def"
+  prefix     = var.prefix
+  name       = "service-${random_id.RANDOM_ID.hex}"
+  repo       = module.ecr.ecr_repository_url
   aws_region = var.aws_region
-  port = 8080
-  s3_arn = module.s3.s3_bucket_arn
-  s3_bucket = module.s3.s3_bucket_id
+  port       = 8080
+  s3_arn     = module.s3.s3_bucket_arn
+  s3_bucket  = module.s3.s3_bucket_id
 }
 
 # service
 module "ecs_service" {
-  source = "./mods/ecs/service"
-  prefix = var.prefix
-  name = "service-${random_id.RANDOM_ID.hex}"
-  desired_count = "1"
-  cluster = module.ecs_cluster.ecs_cluster_id
-  task_definition = module.ecs_task_def.arn_task_definition
-  port = 8080
-  vpc_id = module.net.vpc_id
-  subnets = module.net.private_subnets
-  listener_arn = module.alb.listener_arn
+  source            = "./mods/ecs/service"
+  prefix            = var.prefix
+  name              = "service-${random_id.RANDOM_ID.hex}"
+  desired_count     = "1"
+  cluster           = module.ecs_cluster.ecs_cluster_id
+  task_definition   = module.ecs_task_def.arn_task_definition
+  port              = 8080
+  vpc_id            = module.net.vpc_id
+  subnets           = module.net.private_subnets
+  listener_arn      = module.alb.listener_arn
   listener_priority = 1
 }
 
